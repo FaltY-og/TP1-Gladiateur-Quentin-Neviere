f11b4bf6e0ddefb8a9f90dbe0d9e22ecb45f697b
202330063 <202330063@PO-24865>
2024-09-19 14:11:30 -0400

    Classe du Rétriaire complétée

--- JeuGladiateurs/src/personnages/Retiaire.java
+++ JeuGladiateurs/src/personnages/Retiaire.java
@@ -1,13 +1,23 @@
 
 package personnages;
 
+import java.util.Random;
+
 
 
 public class Retiaire extends Personnage {
+    
+    private boolean filetEnMain;
 
     public Retiaire(String nom, int attaqueMax, int defense, int pvs, int ini) {
         super(nom, attaqueMax, defense, pvs, ini);
+        this.filetEnMain = true;
     }
+
+    public boolean isFiletEnMain() {
+        return filetEnMain;
+    }
+    
     
     @Override
     public void afficherInfosPersonnage() {
@@ -24,5 +34,46 @@ public class Retiaire extends Personnage {
         }
         
     }
-    
+
+    @Override
+    public void frapperPersonnage(Personnage personnageCible) {
+        int dommages;
+        if (filetEnMain) {
+        System.out.println(nom + " lance son filet ");
+            Random rand = new Random();
+            int minValue = 1;
+            int maxValue = 10;
+            int lancerFilet = rand.nextInt(maxValue - minValue) + minValue;
+
+            if (lancerFilet == 1) { //CHANGER VALEUR ICI
+                System.out.println("Son filet attrape " + personnageCible.getNom() + " et il l'empale avec sa lance ! \n");
+                personnageCible.setPointsDeVie(0);
+                filetEnMain = false;
+            } else {
+                System.out.println("Le filet n'a pas atteint sa cible. \n");
+            }
+        }
+        else {
+        System.out.println(nom + " ramasse son filet et en profite pour attaquer.");
+        filetEnMain = true;
+                
+        do {            
+       
+            dommages = attaqueCalcul() - personnageCible.valeurDefense;
+
+        } while (dommages < 0);
+
+        personnageCible.pointsDeVie -= dommages;
+
+        if (personnageCible.pointsDeVie < 0) {
+            personnageCible.pointsDeVie = 0;
+        }
+                
+        System.out.println();
+        System.out.println(this.nom + " attaque avec une puissance de : " + attaqueCalcul());
+        System.out.println(personnageCible.nom + " a une défense de : " + personnageCible.valeurDefense);
+        System.out.println("Les dommages sont donc de : " + dommages);
+   
+        }
+    }
 }
